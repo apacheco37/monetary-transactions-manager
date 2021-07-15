dd0e53030a68b5af997eedc5672a45e42e4a66c7
APacheco37 <apacheco73@hotmail.com>
2021-07-14 23:28:05 -0300

    Add dotenv functionality

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,17 +1,21 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const AppSettings = {
     port: process.env.PORT || 5000,
 }
 
 export const DatabaseSettings = {
-    name: process.env.DBNAME || 'monetarytmdb',
-    username: process.env.DBUSERNAME || 'commander',
-    password: process.env.DBPASSWORD || '3fN3&Q#7nVMW$4zU6WS5',
-    host: process.env.DBHOST || 'monetary-transactions-manager.c9kpuiz5p88j.us-east-2.rds.amazonaws.com',
+    name: process.env.DBNAME || '',
+    username: process.env.DBUSERNAME || '',
+    password: process.env.DBPASSWORD || '',
+    host: process.env.DBHOST || '',
     dialect: process.env.DBDIALECT || 'postgres',
     logging: process.env.DBLOGGING || false
 }
 
 export const FixerSettings = {
-    baseUrl: process.env.FIXERBASEURL || 'http://data.fixer.io/api/latest',
-    accessKey: process.env.FIXERACCESSKEY || 'd65173641e591f2cefb32530a7bf6365'
+    baseUrl: process.env.FIXERBASEURL || '',
+    accessKey: process.env.FIXERACCESSKEY || ''
 }
